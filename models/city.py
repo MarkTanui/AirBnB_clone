ea33ee5de94994a4a01c99b7cc8306f52d6d5bde
Mark Tanui <marktanui75@gmail.com>
2021-11-11 12:21:48 +0300

    debugged console - now running

---
+++ models/city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" class city """
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """
+    public class attributes
+    state_id(str)
+    name(str)
+    """
+
+    state_id = ""
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """ class state
+    name(str) - public class attribute
+    """
+
+    name = ""
